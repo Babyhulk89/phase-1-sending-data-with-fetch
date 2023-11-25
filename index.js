e79728e2f118e8f8896030c38154c792ac4c0af1
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-11-25 16:26:42 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then((response) => response.json())
+      .then((data) => {
+        // Append the new id to the DOM
+        document.body.innerHTML += `<p>ID: ${data.id}</p>`;
+      })
+      .catch((error) => {
+        // Append the error message to the DOM
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
+  
+  // Example usage:
+  submitData("John Doe", "john.doe@example.com");
+  
